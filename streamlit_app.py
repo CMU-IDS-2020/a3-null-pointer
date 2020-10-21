0924404fc60601f7ae03781e7dbdccd709c56fae
Jiecheng Xu <jiechengxu@Jiechengs-MBP.fios-router.home>
2020-10-20 22:59:44 -0400

    first visualization

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,28 +2,52 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
-st.title("Let's analyze some Penguin Data 🐧📊.")
+st.title("Let's analyze some Donald Trump's Tweets!")
 
 @st.cache  # add caching so we load the data only once
 def load_data():
-    # Load the penguin data from https://github.com/allisonhorst/palmerpenguins.
-    penguins_url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/v0.1.0/inst/extdata/penguins.csv"
-    return pd.read_csv(penguins_url)
+    # Load the preprocessed data named 'processed_data.csv' and 'word_count.csv'
+    return pd.read_csv('month_with_wordcount.csv')
+
 
 df = load_data()
 
-st.write("Let's look at raw data in the Pandas Data Frame.")
 
-st.write(df)
+df_filtered = df.copy()
+df_filtered.sentiment = df_filtered.sentiment.map(lambda x: round(x,2))
 
-st.write("Hmm 🤔, is there some correlation between body mass and flipper length? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
+if st.checkbox('Show Raw Data'):
+    st.write(df_filtered)
 
-chart = alt.Chart(df).mark_point().encode(
-    x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
-    y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
-    color=alt.Y("species")
+st.write("Let look at how Trump's sentiment changes over time from 2009-05 to 2020-06")
+st.write("Sentiment is a value between -1 and 1, where -1 stand for 100% negative and 1 stands for 100% positive")
+
+
+brush = alt.selection_interval(encodings = ['x'])
+
+chart = alt.Chart(df_filtered).mark_circle(size=50).encode(
+    x = alt.X('date:T', scale=alt.Scale(zero=False)),
+    y = alt.Y('sentiment:Q'),
+    color = alt.Color('sentiment:Q'),
+    tooltip = ['date:T','sentiment:Q']
 ).properties(
-    width=600, height=400
-).interactive()
+    width=800, height = 600
+)
+
+chart = chart.encode(
+    color = alt.condition(brush, 'sentiment:Q', alt.value('lightgray'))
+).add_selection(brush)
+
 
 st.write(chart)
+# st.write("Hmm 🤔, is there some correlation between body mass and flipper length? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
+#
+# chart = alt.Chart(df).mark_point().encode(
+#     x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
+#     y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
+#     color=alt.Y("species")
+# ).properties(
+#     width=600, height=400
+# ).interactive()
+#
+# st.write(chart)
